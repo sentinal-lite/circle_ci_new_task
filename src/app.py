7e51593f40e812aa53244c37dabc258aa3a51665
sentinal-lite <123827840+sentinal-lite@users.noreply.github.com>
2024-11-04 20:39:38 +0530

    new changes

---
+++ src/app.py
@@ -0,0 +1,23 @@
+def divide(a,b):
+    return a // b
+
+def multiply(a,b):
+    return a * b
+
+def subtract(a,b):
+    return a - b
+
+def add(a,b):
+    return a + b
+
+def main():
+    a = 10
+    b = 5
+    print(add(a, b))
+    print(subtract(a, b))
+    print(multiply(a, b))
+    print(divide(a, b))
+
+
+if __name__ == "__main__":
+    main()
